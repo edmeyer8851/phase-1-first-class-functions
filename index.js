05fff141dfff6edebe8d9156ec4d2da6c21ae812
Ed Meyer <edmeyer8851@gmail.com>
2022-11-21 22:39:52 -0500

    commit tree 9fe6e3c37adbe1a5ee65db998c4b2f964c1137bf

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(cb){
+    cb();
+}
+
+const returnsANamedFunction = () => {
+    return function namedFunction(){};
+}
+
+const returnsAnAnonymousFunction = () => {
+    return function(){};
+}
